46dc5f15510372cb7e0fa186786a1956ea8ac67b
Mansen <husenalasady21@gmail.com>
2023-12-30 17:20:49 +0300

    Remove old comment

--- src/main.cpp
+++ src/main.cpp
@@ -68,7 +68,6 @@ int main()
         glfwTerminate();
         return -1;
     }
-    //sendVertexData();
     float planeVertices[] = {
         // positions          // texture Coords (note we set these higher than 1 (together with GL_REPEAT as texture wrapping mode). this will cause the floor texture to repeat)
          5.0f, -0.5f,  5.0f,  2.0f, 0.0f,
